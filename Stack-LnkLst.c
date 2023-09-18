b92e0234fd7be3afe323713b4677b816014da9d2
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-09-18 11:20:57 +0530

    Updated is-empty function

--- Stack-LnkLst.c
+++ Stack-LnkLst.c
@@ -1,2 +1,21 @@
 //Creating a stack using linked list
 #include<stdio.h>
+
+//creating a structure
+struct node
+    {
+        int data;
+        struct node* link;
+    }  *top = NULL;
+//////////////////////////////////////////////////////
+
+
+//Creating is-empty fuction to check the underflow
+int isEmpty()
+    {
+        if (top == NULL)
+            return 1;
+        else
+            return 0;
+    }
+///////////////////////////////////////////////////////
