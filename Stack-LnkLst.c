f514cd1d456ced506050d611972db09bb74640c3
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-09-19 12:51:10 +0530

    File has been updated

--- Stack-LnkLst.c
+++ Stack-LnkLst.c
@@ -22,3 +22,46 @@ int isEmpty()
 ///////////////////////////////////////////////////////
 
 
+//Creating a push function that is used to push data in the array 
+ void push(int data)
+    {
+        struct node* newNode
+        newNode = malloc(sizeof(newNode));
+
+        if(newNode == NULL)
+            {
+                printf("Stack Overflow");
+                return 0;
+            }
+        newNode -> data = data;
+        newNode -> link = NULL;
+
+        newNode -> link = top;
+        top = newNode;
+    }
+//////////////////////////////////////////////////////////
+
+
+//Creating a pop function that is used to pop out 
+int pop()
+    {
+        struct node* temp;
+        int val;
+        
+        if(isEmpty())
+            {
+                printf("Stack Underflow");
+                return 0;
+            }
+
+        temp = top;
+        val = temp -> data;
+        top = top -> link;
+        free(temp);
+        temp = NULL;
+        return val;
+    }
+//////////////////////////////////////////////////////////////
+
+
+//Creating a peek function that is used to print the top element of the array
