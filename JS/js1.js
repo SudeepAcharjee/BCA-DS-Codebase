1bd9fbe8723393657e2bea4047fc053077634fc6
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-11-27 20:09:58 +0530

    Added File system

---
+++ JS/js1.js
@@ -0,0 +1,19 @@
+//Write a program in javasrcript to accept a name from the user and display the same name in an alert box
+let name = prompt("Enter Your name");
+document.write("Your name is: " + name) ;
+
+
+
+// Write a program in javasrcript to display a message in a confirm box.
+function showMessage() {
+    var message = "Are you Above 18?";
+    var userResponse = window.confirm(message);
+
+    // Check the user's response
+    if (userResponse) {
+        alert("Welcome to 18+ website");
+    } else {
+        alert("Go and watch POGO");
+    }
+}
+showMessage();
